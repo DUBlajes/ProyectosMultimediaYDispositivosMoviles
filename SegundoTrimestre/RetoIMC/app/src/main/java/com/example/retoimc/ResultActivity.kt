08fa5fbea5cd2515e64d7081562eba94555eeb34
Álvaro Guerrero Sánchez <dublajes@gmail.com>
2024-01-26 17:48:02 +0100

    .

--- SegundoTrimestre/RetoIMC/app/src/main/java/com/example/retoimc/ResultActivity.kt
+++ SegundoTrimestre/RetoIMC/app/src/main/java/com/example/retoimc/ResultActivity.kt
@@ -2,10 +2,32 @@ package com.example.retoimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var tvTuResultado:TextView
+    private lateinit var tvResultadoLetras:TextView
+    private lateinit var tvResultadoNumeros:TextView
+    private lateinit var tvMensajeResultado:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val resultado:Double=intent.extras?getDouble(IMC_KEY)?:-1.0
+        initComponents()
+        initUI()
+    }
+
+    private fun initComponents(){
+
+        tvResultadoNumeros=findViewById(R.id.tvResultadoNumeros)
+        tvResultadoLetras=findViewById(R.id.tvResultadoLetras)
+        tvTuResultado=findViewById(R.id.tvTuResultado)
+        tvMensajeResultado=findViewById(R.id.tvMensajeResultado)
+    }
+
+    private fun initUI(){
+
     }
 }
